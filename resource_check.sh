cbe1ad7287c7e3eeccb040c02baf874a44e75a16
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-04-18 00:09:11 +0800

    Update resource_check.sh

--- resource_check.sh
+++ resource_check.sh
@@ -5,15 +5,15 @@ NODENAME=$(kubectl get no -o name | cut -d '/' -f2)
 
 for nn in $NODENAME
 do
-  echo Node:$nn
-  ns=$(kubectl get pods -A -o wide | grep -w Running | grep -w $nn | tr -s \ - | cut -d ' ' -f1 | sort -n | uniq)
+  echo "Node:$nn"
+  ns=$(kubectl get pods -A -o wide | grep -w "Running" | grep -w "$nn" | tr -s \ - | cut -d ' ' -f1 | sort -n | uniq)
   for c in $ns
   do
-    pod=$(kubectl get pods -n $c -o wide | grep -w Running | grep -w $nn | tr -s \ -| cut -d ' ' -f1)
+    pod=$(kubectl get pods -n $c -o wide | grep -w "Running" | grep -w "$nn" | tr -s \ -| cut -d ' ' -f1)
     for p in $pod
     do
       echo "Namespace: $c"
-      kubectl -n $c top pod $p
+      kubectl -n "$c" top pod "$p"
       echo
     done
   done
