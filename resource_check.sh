dfa2e1d7254537c09450912909005cc89c1d4fde
Andy <62133915+cooloo9871@users.noreply.github.com>
2024-04-18 00:04:52 +0800

    Update resource_check.sh

--- resource_check.sh
+++ resource_check.sh
@@ -6,7 +6,7 @@ NODENAME=$(kubectl get no -o name | cut -d '/' -f2)
 for nn in $NODENAME
 do
   echo Node:$nn
-  ns=$(kubectl get pods -A -o wide | grep -w $nn | tr -s \ - | cut -d ' ' -f1)
+  ns=$(kubectl get pods -A -o wide | grep -w Running | grep -w $nn | tr -s \ - | cut -d ' ' -f1 | sort -n | uniq)
   for c in $ns
   do
     pod=$(kubectl get pods -n $c -o wide | grep -w Running | grep -w $nn | tr -s \ -| cut -d ' ' -f1)
